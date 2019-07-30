5c3461ba07baa0b0256697389e34a561b0839af5
Greg <greg@Gregs-MacBook-Pro.local>
2019-07-30 08:39:50 -0500

    files made

---
+++ keys.js
@@ -0,0 +1,6 @@
+console.log('this is loaded');
+
+exports.spotify = {
+  id: process.env.SPOTIFY_70270d8c45704686a9faf2b1110262f9,
+  secret: process.env.SPOTIFY_0ce0f82a1d0b47c0ad8286deb483cdff
+};
