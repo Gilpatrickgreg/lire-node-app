8560e9072a5b51337de285a44daa5d34679889c2
Greg <greg@Gregs-MacBook-Pro.local>
2019-08-02 03:17:53 -0500

    installed npm packages

--- liri.js
+++ liri.js
@@ -1,4 +1,31 @@
 require("dotenv").config();
 var keys = require("./keys.js");
+var axios = require("axios");
+var spotify = require('node-spotify-api');
 var spotify = new Spotify(keys.spotify);
-console.log("this is loaded");
+var movie = process.argv[2]
+axios.get("http://www.omdbapi.com/?i=tt3896198&apikey=46d1379")
+.then(function(response) {
+    
+    console.log(response.data);
+  },
+
+console.log("this is loaded"));
+
+var artist = process.argv[2]
+axios.get("http://www.artists.bandsintown.com/bandsintown-api")
+.then(function(response) {
+    
+    console.log(response.data);
+  },
+
+console.log("this is loaded"));
+
+
+spotify.search({ type: 'track', query: 'All the Small Things' }, function(err, data) {
+    if (err) {
+      return console.log('Error occurred: ' + err);
+    }
+   
+  console.log(data); 
+  });
